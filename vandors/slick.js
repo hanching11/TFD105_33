63ff068a467ae2ff3c660b03ea208b08a6e5c4d1
hanching11 <chfj1111@gmail.com>
2022-02-19 09:15:14 +0800

    nav_all

--- vandors/slick.js
+++ vandors/slick.js
@@ -35,3 +35,19 @@ $(document).on('ready', function() {
   });
   
 });
+
+
+   // 點擊按鈕，選單縮放
+   $("button.hamburger").on("click", function(){$("nav.main_nav").slideToggle();
+            
+  });
+
+
+  // jQuery 版本
+  $(function(){
+  
+  $("button.hamburger").on("click", function(){
+      $(this).toggleClass("is-active");
+  });
+
+  });
